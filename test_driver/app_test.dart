4d85c65a71dd380dac3aca98916b08551a8d413a
JenPed <jennifer.pedraza02@utrgv.edu>
2022-09-19 18:20:25 -0500

    Screen Setup

---
+++ test_driver/app_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+void main() {
+  // FlutterDriver driver;
+
+  // // Connect to the Flutter driver before running any tests.
+  // setUpAll(() async {
+  //   driver = await FlutterDriver.connect();
+  // });
+
+  // // Close the connection to the driver after the tests have completed.
+  // tearDownAll(() async {
+  //   if (driver != null) {
+  //     driver.close();
+  //   }
+  // });
+
+  group('Happy Paths', () {
+    test("", () async {
+      //write the integration test here
+    },skip:true);
+  },skip:true);
+}
