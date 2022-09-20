73d5b8b498c88a826ba5674415d06b3eeaee0882
JenPed <jennifer.pedraza02@utrgv.edu>
2022-09-19 21:05:18 -0500

    Screen Setup

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -1,16 +1,10 @@
-//import 'package:flutter_test/flutter_test.dart';
-//import 'package:integration_test/integration_test.dart';
-import 'package:flutter_driver/flutter_driver.dart';
-import 'package:test/test.dart';
-
-import 'package:seniorproject/main.dart' as app;
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
 
 void main() {
-  //IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized(); // NEW
 
-  group('end-to-end test', () {
-    test('tap on the floating action button, verify counter', () async {
-      //integration tests go here
-    }, skip: true);
-  }, skip: true);
+  testWidgets('failing test example', (tester) async {
+    expect(2 + 2, equals(5));
+  });
 }
